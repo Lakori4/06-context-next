2336d6fae10b4d9526eb986551b7d16eda09a42e
Lakori4 <santivalde04@gmail.com>
2026-03-22 18:04:15 +0100

    removed comment bitching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,14 @@ import { useRouter } from "next/navigation";
 
 const Home = () => {
 
-  const {list} = useList();
+  const { list } = useList();
 
   const router = useRouter();
 
-  /* listPush("string1") I'm an idiot and didn't know that I had to import the function from useList()*/
-
   return (
     <div>
-      <button onClick={()=> {router.push("/addToList")}}>Go to add to list page</button>
-      {list.map (e => (<p key={e}>{e}</p>))}
+      <button onClick={() => { router.push("/addToList") }}>Go to add to list page</button>
+      {list.map(e => (<p key={e}>{e}</p>))}
     </div>
   )
 }
